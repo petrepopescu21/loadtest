bec37fda92a03c77f10a65c833c37c73dc43e265
Petre Popescu <petrepopescu21@gmail.com>
2017-02-07 12:21:09 +0200

    yo

--- server.js
+++ server.js
@@ -5,21 +5,12 @@ var app = Express();
 
 var connectionString='DefaultEndpointsProtocol=https;AccountName=dinistorage;AccountKey=5xs76p+fMf2RaeYKGKtHki5lNxMGcwgEqZ4a8T9CSStAt9fe+krH6oqKNZTqjsbVTQgsG9iUgLl3syTBjxU8PQ==;';
 var queueSvc = azure.createQueueService(connectionString);
-queueSvc.createQueueIfNotExists('nodequeue', function(error, result, response){
-  if(!error){
-    // Queue created or exists
-  }
-});
 
 app.post('/', function(req,res) {
     queueSvc.createMessage('nodequeue', "Hello world!", function(error, result, response){
-        if(!error){
-            res.send(response.isSuccessful);
-        }
-        else {
-            res.send('Sumtin wong');
-        }
+        
     });
+    res.send('ok');
 });
 
 app.get('/', function(req,res) {
