ffae802927265d52fa8ca2f119d0368e30362bfb
Petre Popescu <petrepopescu21@gmail.com>
2017-02-07 12:45:30 +0200

    yo

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-var Express = require('express');
-var azure = require('azure-storage');
-var app = Express();
 
+var azure = require('azure-storage');
+
+
+var http = require('http');
+ 
+http.createServer(function (req, res) {
+    if(req.method == 'POST') {
+         queueSvc.createMessage('nodequeue', "Hello world!", function(error, result, response){
+            console.log('Confirmed');
+        });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+    console.log('----------------');
+    }
+
+
+else {res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, world! [helloworld sample]');}
+    
+}).listen(process.env.PORT || 3000);
 
 var connectionString='DefaultEndpointsProtocol=https;AccountName=dinistorage;AccountKey=5xs76p+fMf2RaeYKGKtHki5lNxMGcwgEqZ4a8T9CSStAt9fe+krH6oqKNZTqjsbVTQgsG9iUgLl3syTBjxU8PQ==;';
 var queueSvc = azure.createQueueService(connectionString);
 
-app.post('/', function(req,res) {
-    queueSvc.createMessage('nodequeue', "Hello world!", function(error, result, response){
-        
-    });
-    res.send('ok');
-});
 
-app.get('/', function(req,res) {
-    res.send('Api working!');
-});
-app.listen(process.env.PORT || 3000);
